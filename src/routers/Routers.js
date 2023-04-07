c5115de4c5e072538e93a57b54fa390083cb1fd8
Mohammad Kardi <mohammadkardi1@gmail.com>
2023-04-07 20:53:55 +0300

    update routes

--- src/routers/Routers.js
+++ src/routers/Routers.js
@@ -18,7 +18,7 @@ export const Routers = () => {
     return (
         <Routes>
             {/* <Route path='/' element={<Navigate to="home"/>}/> */}
-            <Route path='home' element={<Home/>}/>
+            <Route path='/' element={<Home/>}/>
             <Route path='shop' element={<Shop/>}/>
             <Route path='shop/:id' element={<ProductDetails/>}/>
             <Route path='cart' element={<Cart/>}/>
