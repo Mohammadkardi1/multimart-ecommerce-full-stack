46946da6514405b6b1a83440011553226a57a922
MohammadKardi1 <mohammadkardi1@gmail.com>
2024-12-27 20:37:49 +0100

    update cart page

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -12,82 +12,74 @@ import products from './../assets/data/products';
 export const Cart = () => {
 
     const cartItems = products
-    const totalAmount = useSelector(state => state.cart.totalAmount)
+    const totalAmount = 2
     const totalQuantity = 1
-    const dispatch = useDispatch()
+
     const deletehandler = (item) => {
         console.log("Delete Cart ")
     }
 
     return (
-        <Helmet title={"Cart"}>
-            <CommonSection title={'Shopping Cart'}/>
-            <section className="pt-5">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-lg-9">
-                            {cartItems.length === 0 ? <h2 className='fs-4 text-center'>No item added to the cart</h2>
-                                : 
-                                <table className='table bordered'>
-                                    <thead>
-                                        <tr className="fw-bold">
-                                            <td>Image</td>
-                                            <td>Title</td>
-                                            <td>Price</td>
-                                            <td>Qty</td>
-                                            <td>Delete</td>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        {
-                                            cartItems.map((item, index) => (
-                                                <tr key={index}>
-                                                    <td><img src={item.imgUrl} alt="" /></td>
-                                                    <td>{item.productName}</td>
-                                                    <td>{item.price}</td>
-                                                    <td>{item.quantity}</td>
-                                                    <td>
-                                                        <div onClick={() => deletehandler(item)}>
-                                                            <motion.i whileTap={{scale:1.5}}
-                                                            className="ri-delete-bin-line">
-                                                            </motion.i>
-                                                        </div>
-                                                    </td>
-                                                </tr>
-                                            ))
-                                        }
-                                    </tbody>
-                            </table>
-                            }
+    <Helmet title={"Cart"}>
+        <CommonSection title={'Shopping Cart'}/>
+        <section className="pt-5">
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-9">
+                    {cartItems.length === 0 ? <h2 className='fs-4 text-center'>No item added to the cart</h2>
+                    : 
+                    <table className='table bordered'>
+                        <thead>
+                            <tr className="fw-bold">
+                                <td>Image</td>
+                                <td>Title</td>
+                                <td>Price</td>
+                                <td>Qty</td>
+                                <td>Delete</td>
+                            </tr>
+                        </thead>
+                        <tbody>
+                        {cartItems.map((item, index) => (
+                            <tr key={index}>
+                                <td><img src={item.imgUrl} alt="" /></td>
+                                <td>{item.productName}</td>
+                                <td>{item.price}</td>
+                                <td>{item.quantity}</td>
+                                <td>
+                                    <div onClick={() => deletehandler(item)}>
+                                        <motion.i whileTap={{scale:1.5}} className="ri-delete-bin-line"></motion.i>
+                                    </div>
+                                </td>
+                            </tr>
+                        ))}
+                        </tbody>
+                    </table>
+                    }
+                    </div>
+                    <div className="col-lg-3 pt-4 pt-lg-0">
+                        <div>
+                            <h6 className='d-flex align-items-center justify-content-between'>
+                                Subtotal
+                                <span className='fs-4 fw-bold'>${totalAmount}</span>
+                            </h6>
+                            <h6 className='d-flex align-items-center justify-content-between'>
+                                Total Quantity
+                                <span className='fs-4 fw-bold'>{totalQuantity}</span>
+                            </h6>
                         </div>
-                        <div className="col-lg-3 pt-4 pt-lg-0">
-                            <div>
-                                <h6 className='d-flex align-items-center justify-content-between'>
-                                    Subtotal
-                                    <span className='fs-4 fw-bold'>${totalAmount}</span>
-                                </h6>
-                                <h6 className='d-flex align-items-center justify-content-between'>
-                                    Total Quantity
-                                    <span className='fs-4 fw-bold'>{totalQuantity}</span>
-                                </h6>
-                            </div>
-                            <p className='fs-6 mt-2'>taxes and shipping will calculate in checkout</p>
-                            <div>
-                                <button className="shop__btn w-100">
-                                    <Link to='/checkout'>
-                                        Checkout
-                                    </Link>
-                                </button>
-                                <button className="shop__btn w-100 mt-3">
-                                    <Link to='/shop'>
-                                        Continue Shopping
-                                    </Link>
-                                </button>
-                            </div>
+                        <p className='fs-6 mt-2'>taxes and shipping will calculate in checkout</p>
+                        <div>
+                            <button className="shop__btn w-100">
+                                <Link to='/checkout'>Checkout</Link>
+                            </button>
+                            <button className="shop__btn w-100 mt-3">
+                                <Link to='/shop'>Continue Shopping</Link>
+                            </button>
                         </div>
                     </div>
                 </div>
-            </section>
-        </Helmet>
+            </div>
+        </section>
+    </Helmet>
     )
 }
